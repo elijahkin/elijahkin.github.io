6a17b3d15218a07f02dc93cec0537fccc238b7d0
Elijah Kin <elijah.m.kin@gmail.com>
2023-11-07 11:58:17 -0500

    Left and right arrow keys

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-function showImage(imageSrc) {
-  document.getElementById("focused").src = imageSrc;
-  document.getElementById("filter").style.display = "block";
+var focused_idx;
+
+function showImage(idx) {
+  if (idx > 0 && idx <= 16) {
+    focused_idx = idx;
+    document.getElementById("focused").src = "images/" + focused_idx + ".png";
+    document.getElementById("filter").style.display = "block";
+  }
 }
 
 function closeImage() {
@@ -9,8 +14,8 @@ function closeImage() {
 
 document.addEventListener("keyup", (e) => {
   if (e.code === "Escape") closeImage()
-  else if (e.code === "ArrowLeft") closeImage()
-  else if (e.code === "ArrowRight") closeImage()
+  else if (e.code === "ArrowLeft") showImage(focused_idx - 1)
+  else if (e.code === "ArrowRight") showImage(focused_idx + 1)
 });
 
 document.addEventListener('click', (e) => {
