688f2daa71eeaf595866dee263848b2578526f04
Elijah Kin <elijah.m.kin@gmail.com>
2023-11-07 01:34:56 -0500

    Image focusing

---
+++ script.js
@@ -0,0 +1,14 @@
+function showImage(imageSrc) {
+  document.getElementById("focused").src = imageSrc;
+  document.getElementById("filter").style.display = "block";
+}
+
+function closeImage() {
+  document.getElementById("filter").style.display = "none";
+}
+
+document.addEventListener('keyup', (e) => {
+  if (e.code === "Escape") closeImage()
+  else if (e.code === "ArrowLeft") closeImage()
+  else if (e.code === "ArrowRight") closeImage()
+});
