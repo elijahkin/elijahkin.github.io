40015aae8eb3c4f27fb4bdb4ee7a6c6af43c1ad2
Elijah Kin <elijah.m.kin@gmail.com>
2025-02-15 13:45:43 -0500

    Deleting magic squares

--- blog/magic_squares/thingy.py
+++
@@ -1,8 +0,0 @@
-def sextic_residues(n):
-  return set([a**6 % n for a in range(n)])
-
-for n in range(1, 20):
-  print(len(sextic_residues(n)) / n, n, sextic_residues(n))
-
-# res = sextic_residues(72)
-# print(res)
